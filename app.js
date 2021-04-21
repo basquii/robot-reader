43bab462113b2c6b8c3a3856255023e5def259f6
basquii <68734202+basquii@users.noreply.github.com>
2021-04-20 22:18:16 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+const playButton = document.getElementById('play-button')
+const pauseButton = document.getElementById('pause-button')
+const stopButton = document.getElementById('stop-button')
+const textInput = document.getElementById('text')
+const speedInput = document.getElementById('speed')
+const deleteButton = document.getElementById('delete-button')
+
+playButton.addEventListener('click', () => {
+    playText(textInput.value)
+})
+pauseButton.addEventListener('click', pauseText)
+stopButton.addEventListener('click', stopText)
+deleteButton.addEventListener('click', deleteText)
+
+function deleteText() {
+    textInput.value = ""
+}
+
+function playText(text) {
+    if (speechSynthesis.paused && speechSynthesis.speaking) {
+        return speechSynthesis.resume()
+    }
+    const utterance = new SpeechSynthesisUtterance(text)
+    utterance.rate = speedInput.value || 1
+    utterance.addEventListener('end', () => {
+        textInput.disabled = false
+    })
+    textInput.disabled = true
+    speechSynthesis.speak(utterance)
+}
+
+function pauseText() {
+    if (speechSynthesis.speaking) speechSynthesis.pause()
+}
+
+function stopText() {
+    speechSynthesis.cancel()
+}
